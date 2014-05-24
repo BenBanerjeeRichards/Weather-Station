1390117221b24f618cfedceedda113c7561bba9d
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-05-24 15:34:36 +0000

    Added enums

--- c/ws.h
+++ c/ws.h
@@ -3,7 +3,25 @@
 
 // --------- Enum and Struct Definitions --------- //
 
+enum ws_unit_speed
+{
+	METRES_SECOND, KILOMETERS_HOUR, KNOT, MILES_HOUR, BEAUFORT
+};
 
+enum ws_unit_volume 
+{
+	MILLIMETER, INCH
+};
+
+enum ws_unit_temp
+{
+	CELSIUS, FAHRENHEIT
+};
+
+enum ws_unit_pressure
+{
+	HECTOPASCALS, INCH_MERCURY, MILLIMETER_MERCURY
+};
 
 
 // --------- Function Definitions --------- //
