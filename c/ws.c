621f9be8d0b1168082cb919ffe9cd9b91d96818c
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-05-24 20:51:40 +0000

    The beginnings

--- c/ws.c
+++ c/ws.c
@@ -4,8 +4,26 @@
 
 #include "ws.h"
 
-int ws_init()
+int ws_init(ws_device *dev)
 {
+	int status;
+	
+	libusb_set_debug(NULL, LIBUSB_LOG_LEVEL_WARNING);
+	// TODO set logging, and everything else
+	status = libusb_init(NULL);
+	if (status < 0)
+	{
+		printf("[LIBUSB ERROR] %s\n", libusb_error_name(status));
+		return WS_ERR_USB_INIT_FAILED;
+	}
+	
+	dev->hnd = libusb_open_device_with_vid_pid(NULL, 0x1941, 0x8021);
+	if (dev->hnd == NULL)
+	{
+		printf("[LIBUSB ERROR] %s\n", libusb_error_name(status));
+		return WS_ERR_NO_STATION_FOUND;
+	}
+	
 	
 	return 0;
 }
