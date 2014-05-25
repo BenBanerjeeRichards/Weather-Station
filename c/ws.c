13231dde50efaf132408659ab366c17484b5684d
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-05-25 13:24:04 +0000

    Added init function

--- c/ws.c
+++ c/ws.c
@@ -4,26 +4,55 @@
 
 #include "ws.h"
 
+void ws_usb_error(int status, const char* additonal_info)
+{
+	printf("[LIBUSB ERR][ERR NO %i] %s (%s)\n", status, libusb_error_name(status), additonal_info);
+}
+
 int ws_init(ws_device *dev)
 {
 	int status;
 	
-	libusb_set_debug(NULL, LIBUSB_LOG_LEVEL_WARNING);
-	// TODO set logging, and everything else
 	status = libusb_init(NULL);
 	if (status < 0)
 	{
-		printf("[LIBUSB ERROR] %s\n", libusb_error_name(status));
-		return WS_ERR_USB_INIT_FAILED;
+		ws_usb_error(status, "libusb_init");
+		return WS_ERR_USB_INIT_FAILED;  
 	}
 	
-	dev->hnd = libusb_open_device_with_vid_pid(NULL, 0x1941, 0x8021);
-	if (dev->hnd == NULL)
+	// libusb_open_device_with_vid_pid() doesn't seem to work as intended, so 
+	// instead the program loops through all of the devices until the weather station
+	// is found
+	
+	libusb_device **devs;
+	int count = libusb_get_device_list(NULL, &devs);
+	
+	for (int i = 0; i < count; i++)
 	{
-		printf("[LIBUSB ERROR] %s\n", libusb_error_name(status));
-		return WS_ERR_NO_STATION_FOUND;
+		struct libusb_device_descriptor desc;
+		
+		libusb_get_device_descriptor(devs[i], &desc);
+		
+		if (desc.idVendor == 0x1941 && desc.idProduct == 0x8021)
+		{
+			dev->dev = devs[i];
+			dev->desc = &desc;
+			break;
+		}
+		
+		if (i == (count - 1))
+		{
+			return WS_ERR_NO_STATION_FOUND;
+		}
+
 	}
 	
+	status = libusb_open(dev->dev, &dev->hnd);
+	if (status < 0)
+	{
+		ws_usb_error(status, "libusb_open");
+		return WS_ERR_OPEN_FAILED;  
+	}
 	
 	return 0;
 }
