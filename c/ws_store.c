dee40cc1cc7af02609b2a1bb15fdd02fb9a7486f
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-07-15 17:02:36 +0000

    Added files for Writing data to file

---
+++ c/ws_store.c
@@ -0,0 +1,8 @@
+#include "ws_store.h"
+#include <stdio.h>
+
+int ws_store_write_record(ws_weather_record record)
+{
+	// Determine the path
+	return 0;
+}
---
+++ c/ws_store.h
@@ -0,0 +1,8 @@
+#ifndef WS_STORE_H
+#define WS_STORE_H
+
+#include "ws.h"
+
+int ws_store_write_record(ws_weather_record record);
+
+#endif
