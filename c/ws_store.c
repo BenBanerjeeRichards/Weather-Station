57abba9cd07f54b8ee1a64e48ff7fe3f948109ce
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-08-06 14:56:05 +0000

    Added more DB functions

--- c/ws_store.c
+++ c/ws_store.c
@@ -2,36 +2,78 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
-void db_error(struct dbinfo* info, const char* extra)
+void db_error(sqlite3* info, const char* extra)
 {
-	printf("[SQLITE3 ERR] %s (%s)\n",sqlite3_errmsg(info->db_handle), extra);
+	printf("[SQLITE3 ERR] %s (%s)\n",sqlite3_errmsg(info), extra);
 }
 
-int ws_store_open_db(struct dbinfo* info)
+int ws_store_open_db(sqlite3** info)
 {
-	int status = sqlite3_open("WeatherDB.sqlite", &info->db_handle);
+	int status = sqlite3_open_v2("WeatherDB.sqlite", info, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 	if (status != SQLITE_OK)
 	{
-		db_error(info, "ws_store_open_db");
+		db_error(*info, "ws_store_open_db");
 		return WS_ERR_DB_OPEN;
 	}
 
 	return WS_SUCCESS;
 }
 
-int ws_store_close_db(struct dbinfo* info)
+int ws_store_close_db(sqlite3** info)
 {
-	int status = sqlite3_close(info->db_handle);
+	int status = sqlite3_close(*info);
 	if (status != SQLITE_OK)
 	{
-		db_error(info, "ws_store_close_db");
+		db_error(*info, "ws_store_close_db");
 		return WS_ERR_DB_CLOSE;
 	}
 
 	return WS_SUCCESS;
 }
 
-int ws_store_create_statement(struct dbinfo* info, sqlite3_stmt* statement)
+int ws_store_create_statement(sqlite3** info, char* sql, int sql_size, sqlite3_stmt** statement)
 {
-	
+	const char* tail;
+	int status = sqlite3_prepare_v2(*info, sql, sql_size, statement, &tail);
+	if (status != SQLITE_OK)
+	{
+		db_error(*info, "ws_store_create_statement");
+		return WS_ERR_DB_PREPARE;
+	}
+
+	return WS_SUCCESS;
+}
+
+int ws_store_execute_query(sqlite3** info, sqlite3_stmt** statement)
+{
+	int status = sqlite3_step(*statement);
+	if (status == SQLITE_ROW)
+	{
+		return WS_DB_ROW;
+	}
+
+	if (status == SQLITE_MISUSE)
+	{
+		printf("MISUSE!\n");
+	}
+
+	if (status != SQLITE_OK && status != SQLITE_DONE && status != SQLITE_ROW)
+	{
+		db_error(*info, "ws_store_execute_query");
+		return WS_ERR_DB_QUERY;
+	}
+
+	return WS_SUCCESS;
+}
+
+int ws_store_delete_stmt(sqlite3** info, sqlite3_stmt** statement)
+{
+	int status = sqlite3_finalize(*statement);
+	if (status != SQLITE_OK)
+	{
+		db_error(*info, "ws_store_delete_stmt");
+		return WS_ERR_DEL_STMT;
+	}
+
+	return WS_SUCCESS;
 }
--- c/ws_store.h
+++ c/ws_store.h
@@ -4,13 +4,11 @@
 #include "ws.h"
 #include <sqlite3.h>
 
-struct dbinfo {
-	sqlite3* db_handle;
-};
+void db_error(sqlite3*, const char* extra);
 
-void db_error(struct dbinfo* info, const char* extra);
-
-int ws_store_open_db(struct dbinfo* info);
-int ws_store_close_db(struct dbinfo* info);
-int ws_store_create_statement(struct dbinfo* info, const char* sql, int sql_size sqlite3_stmt* statement);
+int ws_store_open_db(sqlite3** info);
+int ws_store_close_db(sqlite3** info);
+int ws_store_create_statement(sqlite3** info, char* sql, int sql_size, sqlite3_stmt** statement);
+int ws_store_execute_query(sqlite3** info, sqlite3_stmt** statement);
+int ws_store_delete_stmt(sqlite3** info, sqlite3_stmt** statement);
 #endif  
